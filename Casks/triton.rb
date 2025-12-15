545406901846248376cccc7a32abd09b77b62f6b
Otávio Cordeiro <otaviocc@users.noreply.github.com>
2025-12-15 21:15:43 +0100

    Add Triton

---
+++ Casks/triton.rb
@@ -0,0 +1,22 @@
+cask "triton" do
+  version "1.0.0"
+  sha256 "7947c6e937e1dc551275dee2e6afc4fec9aa87c10cb4eb546493e7bbb4edc216"
+
+  url "https://github.com/otaviocc/Triton/releases/download/#{version}/OMG.zip"
+  name "Triton"
+  desc "A native macOS client for omg.lol"
+  homepage "https://github.com/otaviocc/Triton"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "OMG.app"
+
+  zap trash: [
+    "~/Library/Application Support/OMG",
+    "~/Library/Caches/OMG",
+    "~/Library/Preferences/com.otaviocc.OMG.plist",
+  ]
+end
