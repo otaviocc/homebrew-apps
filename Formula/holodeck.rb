c366362c8a8efa80071b3c2dff29e9f8ffcbafb5
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2026-05-20 18:48:46 +0200

    Update holodeck to 0.3.0

--- Formula/holodeck.rb
+++ Formula/holodeck.rb
@@ -1,8 +1,8 @@
 class Holodeck < Formula
   desc "macOS CLI and TUI for managing iOS Simulators"
   homepage "https://github.com/otaviocc/Holodeck"
-  url "https://github.com/otaviocc/Holodeck/archive/refs/tags/0.2.0.tar.gz"
-  sha256 "724d2f69e27eb5d6f7595adb525d17489535bb9562b2a50175806e5b90af2a57"
+  url "https://github.com/otaviocc/Holodeck/archive/refs/tags/0.3.0.tar.gz"
+  sha256 "7703cae59551d22252db12e01bd107e0e2e9d6a97397b0a036083bf6d1a78f52"
   license "MIT"
   head "https://github.com/otaviocc/Holodeck.git", branch: "main"
 
