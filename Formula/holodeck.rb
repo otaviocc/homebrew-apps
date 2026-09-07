f5783746c397d6c518ac5d2079ce460e69a7d265
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2026-09-07 17:46:26 +0200

    Bump Holodeck to 0.11.0

--- Formula/holodeck.rb
+++ Formula/holodeck.rb
@@ -1,8 +1,8 @@
 class Holodeck < Formula
   desc "macOS CLI and TUI for managing iOS Simulators"
   homepage "https://github.com/otaviocc/Holodeck"
-  url "https://github.com/otaviocc/Holodeck/archive/refs/tags/v0.10.0.tar.gz"
-  sha256 "7d59d0e3fd59d520437dafcbc2dbc2417d36e1abd884d35416d69377cea4bdfb"
+  url "https://github.com/otaviocc/Holodeck/archive/refs/tags/v0.11.0.tar.gz"
+  sha256 "13093841f40a7225f77c59d217e7e663d57d7d2c19550a716b38d50fb88ede36"
   license "MIT"
   head "https://github.com/otaviocc/Holodeck.git", branch: "main"
 
