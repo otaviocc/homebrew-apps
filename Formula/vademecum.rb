50c83b4dcddf2a727239fdb8423b85ca656ae7bf
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2026-09-10 19:32:46 +0200

    Bump vademecum to 0.6.0

--- Formula/vademecum.rb
+++ Formula/vademecum.rb
@@ -1,8 +1,8 @@
 class Vademecum < Formula
   desc "Themeable terminal Markdown reader with wikilink navigation"
   homepage "https://github.com/otaviocc/vademecum"
-  url "https://github.com/otaviocc/vademecum/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "601b4ff3481d1aad3bd166ef54878b7cc45096a9ccab696c8046950b79e504fe"
+  url "https://github.com/otaviocc/vademecum/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "4a585c0380c00ea30f5c7fd592ee97576cd9da2cf001500f426e5d952029f4a3"
   license "MIT"
   head "https://github.com/otaviocc/vademecum.git", branch: "main"
 
